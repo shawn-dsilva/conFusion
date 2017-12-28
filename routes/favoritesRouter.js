4cea24e0b82e5c21497a26cb6770c7c6dd7786fb
shawn-dsilva <shawn.dsilva_97@protonmail.com>
2017-12-28 18:41:03 +0530

    POST/create functionality completed

--- routes/favoritesRouter.js
+++ routes/favoritesRouter.js
@@ -23,21 +23,22 @@ favoritesRouter.route('/')
             .catch((err) => next(err));
     })
     .post(cors.corsWithOptions,authenticate.verifyUser, (req, res, next) => {
-        Favorites.find({user: req.user._id})
+        Favorites.findOne({user: req.user._id})
             .then((favorite) => {
                 console.log("favorite is : ");
                 console.log(favorite);
+                console.log(typeof(favorite));
                 if (favorite != null) {
                     //favorite.user = req.user._id;
                     //console.log(Favorites.schema.paths);
                     console.log("favorite.dishes before PUSH ")
                     console.log(favorite.dishes);
                     for(var i = 0; i < req.body.length; i++){
-                        favorite[0].dishes.push(req.body[i]._id);
+                        favorite.dishes.push(req.body[i]._id);
                     }
                     console.log("AFTER PUSH OUTPUT FOLLOWS \n");
                     console.log(favorite);
-                    favorite[0].save()
+                    favorite.save()
                     .then((favorite) => {
                         res.statusCode = 200;
                         res.setHeader('Content-Type', 'application/json');
@@ -45,39 +46,20 @@ favoritesRouter.route('/')
                     }, (err) => next(err));
                 }
                 else {
-                    Favorites.create({user : req.user._id});
-                    Favorites.push(req.body[0]._id);
-                    Favorites.save()
+                    Favorites.create({user : req.user._id})
                     .then((favorite) => {
-                        res.statusCode = 200;
-                        res.setHeader('Content-Type', 'application/json');
-                        res.json(favorites); //sends data back in json format                    
-                    }, (err) => next(err));
-                    
-                //     favorite.user = req.user;
-                //     //favorite.favDishes.push(req.body);
-                //     favorite.save()
-                //     .then((favorite) => {
-                //         res.statusCode = 200;
-                //         res.setHeader('Content-Type', 'application/json');
-                //         res.json(favorite); //sends data back in json format 
-                //     // err = new Error('Dish ' + req.params.dishId + ' not found ');
-                //     // err.status = 404;
-                //     // return next(err);//returns to app.js main error handler
-                //     }, (err) => next(err))
-                err = new Error('Dish ' + req.params.dishId + ' not found ');
-                    err.status = 404;
-                    return next(err);//returns to app.js main error handler
-                
-                    }
-                    
-    
-            //     console.log('Dish created ', favorite);
-            //     res.statusCode = 200;
-            //     res.setHeader('Content-Type', 'application/json');
-            //     res.json(favorite);
-            // }, (err) => next(err))
-            // .catch((err) => next(err))           
+                        for(var i = 0; i < req.body.length; i++){
+                            favorite.dishes.push(req.body[i]._id);
+                        }
+                        favorite.save()
+                        .then((favorite) => { 
+                        console.log(favorite);
+                             res.statusCode = 200;
+                             res.setHeader('Content-Type', 'application/json');
+                             res.json(favorite);
+                            },(err) => next(err))              
+                    }, (err) => next(err))    
+                }
     },(err) => next(err))
     .catch((err) => next(err));
     })
